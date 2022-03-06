49ad2259bef4c51b9a4393240f743028cfc517ca
chen <chen@chendeMacBook-Pro-2.local>
2022-03-06 17:51:52 +0800

    update

--- express.js
+++ express.js
@@ -18,8 +18,20 @@ const fonts = [
 
 // express 实例
 const app = express();
-
+app.use((req, res, next) => {
+  //设置请求头
+  res.set({
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Max-Age': 1728000,
+      'Access-Control-Allow-Origin': req.headers.origin || '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+      'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+      'Content-Type': 'application/json; charset=utf-8'
+  })
+  req.method === 'OPTIONS' ? res.status(204).end() : next()
+})
 // 转化参数设置
+app.use(express.json());
 app.use(express.json());
 
 app.use(express.urlencoded({
