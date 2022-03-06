307aceefe2849634fa019ba295d069413207e372
chen <chen@chendeMacBook-Pro-2.local>
2022-03-06 19:55:42 +0800

    update

--- express.js
+++ express.js
@@ -1,76 +1,88 @@
 // const fs = require('fs');
-const fontmin = require('./fontmin')
+const fontmin = require('./fontmin');
 const express = require('express');
 const bodyParser = require('body-parser');
-const findRemoveSync = require('find-remove')
-const path = require('path')
-const os = require("os")
+const findRemoveSync = require('find-remove');
+const path = require('path');
+const os = require('os');
 // 设置可用字体
 const fonts = [
-    {font: 'SourceHanSerifCN-Medium', name: '思源宋体'},
-    {font: 'PangMenZhengDao', name: '庞门正道粗书体'},
-    {font: 'RuiziGongfangMeiheiGBK', name: '锐字工房云字库美黑GBK'},
-    {font: 'RuiziZhenyanti', name: '锐字真言体'},
-    {font: 'YousheBiaotihei', name: '优设标题黑'},
-    {font: 'fandixiaowanzixiaoxueban', name: '新蒂小丸子小学版'},
-    {font: 'Zihun116', name: '字魂116号-凤鸣手书'}
-]
+  { font: 'SourceHanSerifCN-Medium', name: '思源宋体' },
+  { font: 'PangMenZhengDao', name: '庞门正道粗书体' },
+  { font: 'RuiziGongfangMeiheiGBK', name: '锐字工房云字库美黑GBK' },
+  { font: 'RuiziZhenyanti', name: '锐字真言体' },
+  { font: 'YousheBiaotihei', name: '优设标题黑' },
+  { font: 'fandixiaowanzixiaoxueban', name: '新蒂小丸子小学版' },
+  { font: 'Zihun116', name: '字魂116号-凤鸣手书' },
+];
 
 // express 实例
 const app = express();
 app.use((req, res, next) => {
   //设置请求头
-  res.set({
+  const isOps = req.method === 'OPTIONS';
+  if (isOps) {
+    res.set({
       'Access-Control-Allow-Credentials': true,
       'Access-Control-Max-Age': 1728000,
       'Access-Control-Allow-Origin': req.headers.origin || '*',
       'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
       'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
-      'Content-Type': 'application/json; charset=utf-8'
-  })
-  req.method === 'OPTIONS' ? res.status(204).end() : next()
-})
+      'Content-Type': 'application/json; charset=utf-8',
+    });
+    res.status(204).end() ;
+  }else{
+    next();
+  }
+});
 // 转化参数设置
-app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.static(__dirname + '/'));
+app.head('/health/check', function (req, rsp) {
+  rsp.send({ hostname: os.hostname() });
+});
+// post 接口
 app.use(express.json());
 
-app.use(express.urlencoded({
-    extended:true
-}));
-app.use(express.static(__dirname+'/'))
-app.head("/health/check", function(req,rsp){
-  rsp.send({hostname:os.hostname()});
-})
-// post 接口
-app.post('/getfontmin', function(request, response){
-    const params = request.body
-    const font = params.font
-    const text = params.text
-    // 如果传递的font字体在后台没有就返回400
-    const item = fonts.find(e => e.font === font)
-    if(item) {
-        fontmin(font, text, function(e,textHex) {
-            if(e === 'done') {
-               console.log('done')
-               // 拼接参数 返回请求
-               let back = {
-                   url: '/font/' + textHex + "/subset/" + font + '.ttf',
-                   font: font,
-               }
-               response.send(back);
-            }
-        });
-    } else {
-        response.status(400);
-        response.send('没有请求的字体文件');
-    }
+app.post('/getfontmin', function (request, response) {
+  const params = request.body;
+  const font = params.font;
+  const text = params.text;
+  // 如果传递的font字体在后台没有就返回400
+  const item = fonts.find((e) => e.font === font);
+  if (item) {
+    fontmin(font, text, function (e, textHex) {
+      if (e === 'done') {
+        console.log('done');
+        // 拼接参数 返回请求
+        let back = {
+          url: '/font/' + textHex + '/subset/' + font + '.ttf',
+          font: font,
+        };
+        response.json(back);
+      }
+    });
+  } else {
+    response.status(200);
+    response.json({erorr:'没有请求的字体文件'});
+  }
 });
 
 const schedule = require('node-schedule');
+const { json } = require('body-parser');
 // 凌晨5点执行一次清理工作
 schedule.scheduleJob('0 0 5 * * *', () => {
-  console.log('start clear storage ',new Date());
+  console.log('start clear storage ', new Date());
   // 删除一天前的文件
-  findRemoveSync(path.join(__dirname,'font'), {age: {seconds: 86400}, limit: 100, dir: '*'})
+  findRemoveSync(path.join(__dirname, 'font'), {
+    age: { seconds: 86400 },
+    limit: 100,
+    dir: '*',
+  });
 });
 app.listen(3000);
